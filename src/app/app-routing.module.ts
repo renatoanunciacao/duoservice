1aa5d271df15166b66c3b1e8d4ec09ac334c1095
Renato Anunciação <wagnerpunk002@gmail.com>
2020-07-19 22:19:20 -0300

    criação da page login, page inicio

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+  {
+    path: "",
+    loadChildren: () =>
+      import("./tabs/tabs.module").then((m) => m.TabsPageModule),
+  },
+  {
+    path: "login",
+    loadChildren: () =>
+      import("./login/login.module").then((m) => m.LoginPageModule),
+  },
+  {
+    path: "inicio",
+    loadChildren: () =>
+      import("./inicio/inicio.module").then((m) => m.InicioPageModule),
+  },
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ src/app/tabs/tabs-routing.module.ts
@@ -0,0 +1,48 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { TabsPage } from "./tabs.page";
+
+const routes: Routes = [
+  {
+    path: "tabs",
+    component: TabsPage,
+    children: [
+      {
+        path: "login",
+        loadChildren: () =>
+          import("../login/login.module").then((m) => m.LoginPageModule),
+      },
+      {
+        path: "inicio",
+        loadChildren: () =>
+          import("../inicio/inicio.module").then((m) => m.InicioPageModule),
+      },
+      {
+        path: "tab2",
+        loadChildren: () =>
+          import("../tab2/tab2.module").then((m) => m.Tab2PageModule),
+      },
+      {
+        path: "tab3",
+        loadChildren: () =>
+          import("../tab3/tab3.module").then((m) => m.Tab3PageModule),
+      },
+      {
+        path: "",
+        redirectTo: "/tabs/login",
+        pathMatch: "full",
+      },
+    ],
+  },
+  {
+    path: "",
+    redirectTo: "/tabs/login",
+    pathMatch: "full",
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class TabsPageRoutingModule {}
